6233400113a82eb1308f41c81172520087caf90e
ya215 <82383252+ya215@users.noreply.github.com>
2024-11-09 12:01:55 +0100

    run_preprocessing.py

--- milestone_1/run_preprocessing.py
+++ milestone_1/run_preprocessing.py
@@ -1,15 +1,27 @@
 import sys
+import pandas as pd
 
 sys.path.append('..')
-from utils.preprocessing import prepare_full_dataset, prepare_split_datasets,load_processed_data
+from utils.preprocessing import (
+    prepare_full_dataset,
+    prepare_split_datasets,
+    load_processed_data,
+    read_conllu_file  # Import the new function
+)
+
 
 processed_df, conllu_format = prepare_full_dataset()
+print("Processed DataFrame:")
 print(processed_df)
+
+
 split_datasets = prepare_split_datasets()
 
+
 split_dfs = load_processed_data(split=['train', 'dev', 'test'])
+print("\nTrain DataFrame:")
 print(split_dfs["train"])
+print("\nDev DataFrame:")
 print(split_dfs["dev"])
+print("\nTest DataFrame:")
 print(split_dfs["test"])
-
-
